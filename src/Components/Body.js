4a8e026bf27dc2152194f12802827e7dadc6cc47
drkspark <madhusudhanrock753@gmail.com>
2023-07-04 22:06:43 +0530

    Added Card Component and Track Div

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,7 +1,19 @@
+import Card from "./Card";
+import { CardData } from "../constants";
+
+console.log(CardData);
 export default function Body() {
     return (
         <div className='body'>
-            <div className='tracks'>Here we will have Track Cards</div>
+            <div className='tracks'>
+                {CardData.map((card, idx) => {
+                    return (<Card
+                        key={idx}
+                        title={card.title}
+                        img_url={card.url}
+                    />)
+                })}
+            </div>
             <div className='effects'>Here we will have Effect Cards</div>
         </div>
     );
---
+++ src/constants.js
@@ -0,0 +1,28 @@
+import img1 from "./assets/backgroud/1.gif";
+import img2 from "./assets/backgroud/2.gif";
+import img3 from "./assets/backgroud/3.gif";
+import img4 from "./assets/backgroud/4.gif";
+import img5 from "./assets/backgroud/5.gif";
+
+export const CardData = [
+    {
+        url: img1,
+        title: "Silent Wood",
+    },
+    {
+        url: img2,
+        title: "Lost And Found",
+    },
+    {
+        url: img3,
+        title: "Celestia",
+    },
+    {
+        url: img4,
+        title: "Otjanbird",
+    },
+    {
+        url: img5,
+        title: "Crescent Mood",
+    },
+];
