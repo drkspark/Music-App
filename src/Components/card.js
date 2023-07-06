e0873676bfb299b9225b05dbf5ea814c88e35458
banothabhinay <bpream01@gmail.com>
2023-07-06 16:26:59 +0530

    Main

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,30 +1,16 @@
 import React from "react";
-import pic1 from "../assets/backgroud/1.gif";
-import pic2 from "../assets/backgroud/2.gif";
-export default function Card() {
-    return(
-<div className="frames">
-    <div className="cards">
-        <a href='#'>
-        <img
-            className='pic_1'
-            alt='pic__1'
-            src={pic1}
-            href='#'
-        />
-        </a>  
-    </div>
-    <div className="cards">
-        <a href='#'>
-        <img
-            className='pic_2'
-            alt='pic__2'
-            src={pic2}
-            href='#'
-        />
-        </a>  
-    </div>
 
-</div>
+// Naming Convention: CardMember, CardBody
+export default function Card(props) {
+    const { title, img_url } = props;
+
+    return (
+        <div className='card'>
+            <img
+                className='card-img'
+                src={img_url}
+            />
+            <div class='bottom-left'>{title}</div>
+        </div>
     );
-};
+}
---
+++ src/constants.js
@@ -0,0 +1,24 @@
+import pic1 from "./assets/backgroud/1.gif";
+import pic2 from "./assets/backgroud/2.gif";
+import pic3 from "./assets/backgroud/3.gif";
+import pic4 from "./assets/backgroud/4.gif";
+import pic5 from "./assets/backgroud/5.gif";
+
+export const CardData=[
+        {
+            url:pic1,title:'Silent Wood',
+        },
+        {
+            url:pic2,title:'Lost And Found',
+        },
+        {
+            url:pic3,title:'Celestia',
+        },
+        {
+            url:pic4,title:'Otjanbird',
+        },
+        {
+            url:pic5,title:'Crescent Mood',
+        },
+
+];
