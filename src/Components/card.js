344192011753d7582af59d6bbd8086d1d88b31e9
banothabhinay <bpream01@gmail.com>
2023-07-16 21:28:33 +0530

    done

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,39 +1,3 @@
-/*import React, { useState } from "react";
-
-// Naming Convention: CardMember, CardBody
-export default function Card(props) {
-    const [song, setSong] = useState();
-    const [playing, setPlaying] = useState(false);
-
-    const { title, img_url, sound_track } = props;
-    return (
-        <div
-            className='card'
-            onClick={() => {
-                if (playing === false) {
-                    setSong(sound_track);
-                    setPlaying(true);
-                } else {
-                    setSong();
-                    setPlaying(false);
-                }
-            }}
-        >
-            <img
-                className='card-img'
-                src={img_url}
-            />
-            <div class='bottom-left'>{title}</div>
-            {playing && (
-                <audio
-                    src={song}
-                    autoPlay
-                    controls
-                />
-            )}
-        </div>
-    );
-}*/
 import React, { useState } from "react";
 
 // Naming Convention: CardMember, CardBody
