0e890138f0e065b933b6ae5b3e9c9ccfc73b9e72
banothabhinay <bpream01@gmail.com>
2023-07-30 16:29:59 +0530

    done

--- src/Components/card.js
+++ src/Components/card.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
 // Naming Convention: CardMember, CardBody
-export default function Card(props) {
-    const { title, img_url, play } = props;
 
+export default function Card(props) {
+    const { title, img_url,cname, play} = props;
+    
     return (
         <div className='card' onClick={play}>
-            <img className='card-img' src={img_url} />
+            <img className={cname} src={img_url} />
             <div className='bottom-left'>{title}</div>
         </div>
     );
