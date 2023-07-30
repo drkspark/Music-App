67ca9b4448e34266393f199084c29a03339a8e2b
banothabhinay <bpream01@gmail.com>
2023-07-30 16:30:35 +0530

    css updated

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,39 +2,56 @@ import Navigation from "./NavigationMenu";
 import Card from "./Card";
 import { CardData } from "../Constants/constants";
 import { EffectData } from "../Constants/effectsconstants";
-import React, { useState } from "react";
-
+import React, { useEffect, useLayoutEffect, useState } from "react";
+ganne='card-imgg';
+gane='card-img';
+const [handle,setHandle]=useState("");
 export default function Body() {
     const [song, setSong] = useState();
     const [playing, setPlaying] = useState(false);
 
     const [effect, setEffect] = useState();
     const [effectPlaying, setEffectPlaying] = useState(false);
-
     const songPlayer = (sound_track) => {
         if (playing && song != sound_track) {
             setSong(sound_track);
+           
         } else if (playing === false) {
             setSong(sound_track);
             setPlaying(true);
+           
+            
         } else {
             setSong();
             setPlaying(false);
+          
+            
         }
     };
 
+    
     const effectPlayer = (effect_track) => {
         if (effectPlaying && effect != effect_track) {
             setEffect(effect_track);
+
         } else if (effectPlaying === false) {
             setEffect(effect_track);
             setEffectPlaying(true);
+            
         } else {
             setEffect();
             setEffectPlaying(false);
         }
     };
 
+const handleRim=()=>{
+    if(handle==='card-img'){
+        setHandle('card-imgg');
+    }
+    else{
+        setHandle('card-img');
+    }
+    };
     const pause = () => {
         setPlaying(false);
     };
@@ -50,9 +67,12 @@ export default function Body() {
                             key={idx}
                             title={card.title}
                             img_url={card.url}
+                            cname={handle}
                             play={() => {
                                 songPlayer(card.sound_track);
+                                handleRim();
                             }}
+                            
                         />
                     );
                 })}
@@ -66,9 +86,11 @@ export default function Body() {
                             key={id}
                             title={card.title}
                             img_url={card.url}
+                            cname={handle}
                             sound_track={card.effect_track}
                             play={() => {
                                 effectPlayer(card.effect_track);
+                               handleRim();
                             }}
                         />
                     );
